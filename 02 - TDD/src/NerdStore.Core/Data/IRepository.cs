d952f0c00d27117ac34f0034823ed3cb71487a6d
henrique <rosa.henrique@outlook.com.br>
2024-03-30 16:59:38 -0300

    finalizado

---
+++ 02 - TDD/src/NerdStore.Core/Data/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Core.Data;
+
+public interface IRepository<T> : IDisposable where T : IAggregateRoot
+{
+    IUnitOfWork UnitOfWork { get; }
+}
---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/TipoDescontoVoucher.cs
@@ -0,0 +1,6 @@
+﻿namespace NerdStore.Vendas.Domain;
+public enum TipoDescontoVoucher
+{
+    Porcentagem = 0,
+    Valor = 1
+}
